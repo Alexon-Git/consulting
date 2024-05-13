ffd3a4fcb37051c28833e0426419929504d094fb
unknown <Saakh@DESKTOP-2NU7BDI.com>
2024-05-13 13:18:35 +0300

    finish

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import './header.css'
+import React, { useState } from "react";
+import "./header.css";
 
 const Header = () => {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  // Функция для открытия/закрытия мобильного меню
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
   return (
     <header>
       <div className="main-container">
@@ -12,7 +19,7 @@ const Header = () => {
           <ul className="nav_list">
             <li>
               <a className="menu_link" href="#!">
-                услуги            
+                услуги
               </a>
             </li>
             <li>
@@ -32,14 +39,58 @@ const Header = () => {
             </li>
           </ul>
 
-
-
           <div className="nav_btn">
-            <a className="btn_white" href="#!">Контакты</a>
+            <a className="btn_white" href="#!">
+              Контакты
+            </a>
           </div>
 
+          <div class="nav__icon">
+            <button class="mobile-nav-btn" onClick={toggleMobileNav}>
+            {isMobileNavOpen ? (
+            <img className="nav-icon" src="./img/icons/close_icon.svg" alt="Close" />
+          ) : (
+            <img className="nav-icon" src="./img/icons/mobile_nav.svg" alt="Open" />
+          )}
+            </button>
+          </div>
         </nav>
       </div>
+      {isMobileNavOpen && (
+        <div className="mobile-nav-overlay">
+
+        <div className="mobile-nav">
+          <ul>
+            <li>
+              <a className="menu_link" href="#!">
+                услуги
+              </a>
+            </li>
+            <li>
+              <a className="menu_link" href="#!">
+                кейсы
+              </a>
+            </li>
+            <li>
+              <a className="nav_link" href="#!">
+                клиенты
+              </a>
+            </li>
+            <li>
+              <a className="nav_link" href="#!">
+                команда
+              </a>
+            </li>
+
+            <div className="nav_btn_mobile">
+              <a className="btn_white-mobile" href="#!">
+                Контакты
+              </a>
+            </div>
+          </ul>
+        </div>
+        </div>
+      )}
     </header>
   );
 };
